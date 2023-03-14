c1f5342605f1798124e0084e400de28077076334
pratapbaraiyagit <121426664+pratapbaraiyagit@users.noreply.github.com>
2023-03-14 16:27:21 +0530

    Add files via upload

---
+++ src/Assets/AdminPanel/index.js
@@ -0,0 +1,26 @@
+import img_profile from "./profile.svg"
+import img_logo from "./logo.svg"
+import img_tools from "./tool.svg"
+import img_mainLogo from "./mainLogo.svg"
+import img_loginBg from "./loginBg.svg"
+import img_profileIcon from "./default_profile.svg"
+import img_camera from "./camera.svg"
+import img_ring from "./ring.png"
+import img_ring1 from "./ring1.png"
+import img_orders from "./orders.png"
+import img_partners from "./partners.png"
+import img_customers from "./customers.png"
+export {
+    img_profile,
+    img_logo,
+    img_tools,
+    img_mainLogo,
+    img_loginBg,
+    img_profileIcon,
+    img_camera,
+    img_ring,
+    img_ring1,
+    img_orders,
+    img_customers,
+    img_partners
+}
